a674b28f4e33d92e0af8eab443a741a304b1d315
lrz <rongzhi.liu@evervan.com.cn>
2017-11-01 17:09:10 +0800

    ignore

--- Treasure.Model/SmallTool/DataSynchron/DataSynchronVO.cs
+++ Treasure.Model/SmallTool/DataSynchron/DataSynchronVO.cs
@@ -8,9 +8,15 @@ namespace Treasure.Model.SmallTool.DataSynchron
 {
     public static class DataSynchronVO
     {
+        //用于数据库链接
         public static string Ip = "IP";
         public static string LoginName = "LOGIN_NAME";
         public static string Pwd = "PASSWORD";
         public static string DbName = "DATABASE_NAME";
+
+        //用于表结构
+        public static string TableName = "TableName";
+        public static string Description = "Description";
+
     }
 }
